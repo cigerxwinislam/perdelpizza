4d80e12f25ec0ea95f07c087a713875bab6ff0d4
Yekazad Software Center <91857222+cigerxwinislam@users.noreply.github.com>
2024-09-22 16:20:07 +0300

    serxerenu

--- perdelpizza/app/page.tsx
+++ perdelpizza/app/page.tsx
@@ -4,11 +4,33 @@
 
 import React from 'react'
 import KitchenSinkExample from './components/card'
-import mmv from '../public/mmv.png'
+import mm1 from '../public/mm1.JPG'
 
-import mme from '../public/mmmedit.png'
+import mm2 from '../public/mm2.JPG'
 
-import mma from '../public/mmauber.png'
+import mm3 from '../public/mm3.JPG'
+
+import mm4 from '../public/mm4.JPG'
+
+import mm5 from '../public/mm5.JPG'
+
+import mm6 from '../public/mm6.JPG'
+
+import mm7 from '../public/mm7.JPG'
+
+import mm8 from '../public/mm8.JPG'
+
+import mm9 from '../public/mm9.JPG'
+
+import mm10 from '../public/mm10.JPG'
+
+import mm11 from '../public/mm21.JPG'
+
+import mm12 from '../public/mm17.JPG'
+
+import mm13 from '../public/mm13.JPG'
+
+import mm14 from '../public/mm14.JPG'
 import { Container,Row,Col, Alert } from 'react-bootstrap'
 
 function page() {
@@ -20,53 +42,42 @@ function page() {
 
       </Alert>
 
-<Alert style={{background:"orange",}}>
 
-<h5>1.Kategori</h5> <p>...</p>
-
-     </Alert>
 
 <Row>
 
 <Col><br></br>
 
-<KitchenSinkExample wene={mmv} nav="Veggie"/>
+<KitchenSinkExample wene={mm1} nav="Delicious Pizza"/>
     
-  <br></br> <br></br>  <KitchenSinkExample wene={mma} nav="Auber"/>
+  <br></br> <br></br>  <KitchenSinkExample wene={mm3} nav="Beef Soecial"/>
 
 </Col>
 
 <Col><br></br>
 
-<KitchenSinkExample wene={mmv} nav="Veggie"/>
+<KitchenSinkExample wene={mm5} nav="Pesto Mix Special"/>
     
-<br></br> <br></br><KitchenSinkExample wene={mma} nav="Auber"/>
+<br></br> <br></br><KitchenSinkExample wene={mm12} nav="Mexsicano"/>
 
 </Col>
 
 </Row>
 <Alert style={{background:"orange",}}>
 
-<h5>2.Kategori</h5> <p>...</p>
 
      </Alert>
      <Row>
 
 <Col><br></br>
 
-<KitchenSinkExample wene={mmv} nav="Veggie"/>
+<KitchenSinkExample wene={mm11} nav="Akdeniz"/>
     
-  <br></br> <br></br>  <KitchenSinkExample wene={mma} nav="Auber"/>
+  <br></br> <br></br>  <KitchenSinkExample wene={mm13} nav="Cosmo"/>
 
 </Col>
 
-<Col><br></br>
 
-<KitchenSinkExample wene={mmv} nav="Veggie"/>
-    
-<br></br> <br></br><KitchenSinkExample wene={mma} nav="Auber"/>
-
-</Col>
 
 </Row>
 
