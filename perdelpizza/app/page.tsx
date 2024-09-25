65d5d1e3a859651c5cc2fc7ade880e6aaf01eb59
Yekazad Software Center <91857222+cigerxwinislam@users.noreply.github.com>
2024-09-25 14:14:21 +0300

    serxere nu

--- perdelpizza/app/page.tsx
+++ perdelpizza/app/page.tsx
@@ -4,9 +4,81 @@
 
 import React from 'react'
 
+import { Row,Container,Col } from 'react-bootstrap'
+
+import KitchenSinkExample from './components/card'
 function page() {
   return (
-    <div>page</div>
+    <div>
+
+
+
+
+<Container>
+
+<Row>
+
+<Col>
+
+<KitchenSinkExample nav="Mexicano"/>
+
+<KitchenSinkExample nav="Margherita"/>
+
+</Col>
+
+<Col>
+
+<KitchenSinkExample nav="Cosmo"/>
+
+<KitchenSinkExample nav="Pesto Mix Special"/>
+
+</Col>
+
+</Row>
+
+<Row>
+
+<Col>
+
+<KitchenSinkExample nav="Beef Soecial"/>
+
+<KitchenSinkExample nav="Tırkish Special"/>
+
+</Col>
+
+<Col>
+
+<KitchenSinkExample nav="Tonton"/>
+
+<KitchenSinkExample nav="Chicken Pesto"/>
+
+</Col>
+
+</Row>
+
+<Row>
+
+<Col>
+
+<KitchenSinkExample nav="Delicious Pizza"/>
+
+<KitchenSinkExample nav="Chicken Bbq"/>
+
+</Col>
+
+<Col>
+
+<KitchenSinkExample nav="Bay Frodo"/>
+
+<KitchenSinkExample nav="Veggie"/>
+
+</Col>
+
+</Row>
+</Container>
+
+
+    </div>
   )
 }
 
