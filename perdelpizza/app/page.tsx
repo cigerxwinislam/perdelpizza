9c002ef09ed37a8787a3df9dd1ca872b8bd4c56a
Yekazad Software Center <91857222+cigerxwinislam@users.noreply.github.com>
2024-10-28 22:13:33 +0300

    serxere nu

--- perdelpizza/app/page.tsx
+++ perdelpizza/app/page.tsx
@@ -14,41 +14,19 @@ import KitchenSinkExample from './components/card'
 
 import mm1 from '../image/mm5.jpg'
 
-import mb2 from '../public/mb10.jpg'
-
-import mb3 from '../public/mb11.jpg'
-
-import mb4 from '../public/mb12.jpg'
-
-import mb5 from '../public/mb13.jpg'
-
-import mb6 from '../public/mb14.jpg'
-
-import mb7 from '../public/mb15.jpg'
-
-import mb8 from '../public/mb16.jpg'
-
-import mb9 from '../public/mb17.jpg'
+import mm9 from '../image/mm9.png'
+import mm13 from '../image/mm13.jpg'
+import mm15 from '../image/mm15.jpg'
 
 
 
-import mb11 from '../public/mb18.jpg'
+import mm17 from '../image/mm17.jpg'
+import mm19 from '../image/mm19.jpg'
+import mm23 from '../image/mm23.jpg'
 
-import mb13 from '../public/mb1.jpg'
 
-import mb15 from '../public/mb2.jpg'
 
-import mb17 from '../public/mb3.jpg'
 
-import mb19 from '../public/mb4.jpg'
-
-import mb21 from '../public/mb5.jpg'
-
-import mb23 from '../public/mb6.jpg'
-
-import mb25 from '../public/mb13.jpg'
-
-import mb27 from '../public/mb8.jpg'
 
 
 
@@ -75,21 +53,21 @@ function page() {
 
   <Col><KitchenSinkExample nav="" wene={mm1} />
 
-<KitchenSinkExample nav="" />
+<KitchenSinkExample nav="" wene={mm9}/>
 
-<KitchenSinkExample nav="" />
+<KitchenSinkExample nav="" wene={mm13} />
 
-<KitchenSinkExample nav="" />
+<KitchenSinkExample nav="" wene={mm15}/>
 
 </Col>
 
 
 
-  <Col><KitchenSinkExample nav="" />
+  <Col><KitchenSinkExample nav="" wene={mm17}/>
 
-<KitchenSinkExample nav="" />
+<KitchenSinkExample nav="" wene={mm19} />
 
-<KitchenSinkExample nav="" />
+<KitchenSinkExample nav="" wene={mm23}/>
 
 <KitchenSinkExample nav="" />
 
